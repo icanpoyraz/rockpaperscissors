d057ee08435c3d5be1b21f36da42a1142f10e2c5
icanpoyraz <54509677+icanpoyraz@users.noreply.github.com>
2022-12-12 00:04:35 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title("Rock, Paper, Scissors")
 st.write('by Ihsancan Ozpoyraz - 2022')
+
+st.write('This mini-game was programmed to illustrate the concept of decision-making under uncertainty in the blog post titled Rock, Paper and Scissors: Which one to go with?')
+st.write('Here is the link for the blog post: [link here]')
